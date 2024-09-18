a8b75ce6ed3c12efe904a9d6f45116fa8e2271ab
Snehabelek <snehabelekar861@gmail.com>
2024-09-18 13:01:11 +0530

    Add files via upload

---
+++ BinaryPattern.java
@@ -0,0 +1,11 @@
+public class BinaryPattern {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print((i + j) % 2 + " ");
+            }
+            System.out.println();
+        }
+    }
+}
